491a26eb19c1143697074c052565c97cb73fd698
Edward McCarthy <efmccarthy26@gmail.com>
2017-09-01 12:48:02 -0600

    Now adding tick event where these two cells die

---
+++ spec/live_cell_rules_spec.rb
@@ -0,0 +1,26 @@
+require "rspec"
+require "pry"
+require "./app/world"
+require "./app/cell"
+require "./app/board"
+
+describe "LiveCellRules" do
+  let(:board) { Board.new }
+  let(:world) { World.new }
+
+  context "When a cell is alive" do
+    describe "Has fewer than two live neighbors" do
+      it "results results in death" do
+        board = Board.new(world, [[1,1],[2,0]])
+        # binding.pry
+        board.tick!
+        # Both only have each other, so dead
+        c1 = world.grid[2][0]
+        c2 = world.grid[1][1]
+
+        expect(c1).to_not be_alive
+        expect(c2).to_not be_alive
+      end
+    end
+  end
+end
