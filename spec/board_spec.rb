f42fb8c8c52e7c43fa260ed840d9c3e3c495a0c6
Edward McCarthy <efmccarthy26@gmail.com>
2017-09-01 12:15:00 -0600

    some confusion: I've been creating cells with (x,y) coordinates, but seeding them into the world (to World class via Board) in a form of (col,row) so I'm confused about placement. I don't think I'll change this... The board drawinghelps, and pry will keep giving me a visual of changes

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,11 +23,9 @@ describe "Board" do
     it "should seed cells across grid" do
       b1 = Board.new(world, [[1,1],[2,0]])
       cell1 = world.grid[1,1].first[1]
-      #
-      cell2 = world.grid[2,0]
-      # binding.pry
+      cell2 = world.grid[2][0]
       expect(cell1).to be_alive
-      # expect(cell2).to be_alive
+      expect(cell2).to be_alive
     end
   end
 end
