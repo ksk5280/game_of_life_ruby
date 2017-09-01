9f164a14b436be922d3de4b1154c326cfb7e092a
Edward McCarthy <efmccarthy26@gmail.com>
2017-09-01 12:59:32 -0600

    maybe need a dead method

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,13 +5,12 @@ require "./app/world"
 # The purpose of “describe” is to wrap a set of tests against one functionality while “context” is to wrap a set of tests against one functionality under the same state.
 
 describe "World" do
-  describe "when initialized" do
-    subject { World.new }
+  subject { World.new }
 
+  describe "when initialized" do
     it "creates a world of columns and rows" do
       expect(subject).to respond_to(:rows)
       expect(subject).to respond_to(:columns)
-      expect(subject).to respond_to(:live_neighbors)
     end
 
     context "creates a grid" do
@@ -25,7 +24,19 @@ describe "World" do
           expect(row).to be_a(Array)
         end
       end
+    end
+  end
 
+  describe "should be aware of the cell neighbors and living conditions" do
+    it "knows of the method" do
+      expect(subject).to respond_to(:live_neighbors)
+    end
+
+    it "detects northern neighbors" do
+      north_cell = subject.grid[0][1]
+      expect(north_cell).to_not be_alive
+      north_cell.alive = true
+      expect(north_cell).to be_alive
     end
   end
 end
