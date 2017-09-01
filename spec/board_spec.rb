6cde45fb374abc89bdb35bf00dda0c2836614ba8
Edward McCarthy <efmccarthy26@gmail.com>
2017-09-01 12:07:09 -0600

    making a single cell alive, but seems like I've added an extra unnecessary layer

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,8 +7,8 @@ require "./app/board"
 describe "Board" do
   subject { Board.new }
 
-  describe "is initialized" do
-    # let(:world) { World.new }
+  describe "is built and" do
+    let(:world) { World.new }
     # let(:cell)  { Cell.new}
 
     it "should create a new board game object" do
@@ -16,9 +16,18 @@ describe "Board" do
     end
 
     it "has proper knowledge of methods" do
-      # binding.pry
       expect(subject).to respond_to(:world)
       expect(subject).to respond_to(:cells)
+    end
+
+    it "should seed cells across grid" do
+      b1 = Board.new(world, [[1,1],[2,0]])
+      cell1 = world.grid[1,1].first[1]
+      #
+      cell2 = world.grid[2,0]
+      # binding.pry
+      expect(cell1).to be_alive
+      # expect(cell2).to be_alive
     end
   end
 end
