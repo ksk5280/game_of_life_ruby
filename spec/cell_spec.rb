24443bee8056ae67d505de5dd39c57b1741f1079
Edward McCarthy <efmccarthy26@gmail.com>
2017-09-06 08:02:43 -0600

    removed skipped tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,18 +8,6 @@ describe "Cell" do
   let(:world) { World.new }
 
   describe "when initialized" do
-    xit "should equal new cell object" do
-      expect(Cell.new).to be_a(subject)
-    end
-
-    xit "is a part of a world" do
-      world.grid.each do |row|
-        row.each do |col|
-          expect(col).to be(subject)
-        end
-      end
-    end
-
     it "has a location attributes" do
       expect(subject).to respond_to(:x)
       expect(subject).to respond_to(:y)
